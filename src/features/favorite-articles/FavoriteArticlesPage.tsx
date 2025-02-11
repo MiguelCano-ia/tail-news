1abe401d8b840e5bf8ffc0b04ace74db65d69f84
Miguel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2025-02-11 14:26:38 -0500

    Fixed: undefined uri

--- src/features/favorite-articles/FavoriteArticlesPage.tsx
+++ src/features/favorite-articles/FavoriteArticlesPage.tsx
@@ -25,14 +25,21 @@ const FavoriteArticlesPage = () => {
 
   if (!articles) return <NoFavoriteArticles />;
 
+  const exitsArticles = articles.filter((arcicle) => arcicle !== undefined);
+
   return (
     <>
       <div className="container m-auto flex flex-col items-center">
-        <div className="text-3xl font-bold mb-10 text-center mt-10">
-          My Favorite Articles
+        <div className="mb-10 mt-10">
+          <div className="text-3xl font-bold text-center">
+            My Favorite Articles
+          </div>
+          <small>
+            Some items may not appear because they were removed some time later.
+          </small>
         </div>
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 px-10 lg:px-20 mb-20">
-          {articles.map(({ uri, title, image, dateTime, authors }) => (
+          {exitsArticles.map(({ uri, title, image, dateTime, authors }) => (
             <NewsCard
               key={uri}
               uri={uri}
